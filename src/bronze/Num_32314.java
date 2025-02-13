83f7ccbcbdedc7ac98882313225e433e09084dc6
jimin990 <wlals3236@naver.com>
2025-02-13 21:42:43 +0900

    marathon start

---
+++ src/bronze/Num_32314.java
@@ -0,0 +1,26 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Num_32314 {
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int tree_ampere = Integer.parseInt(br.readLine());
+
+        String[] secondLine = br.readLine().split(" ");
+
+        int watt = Integer.parseInt(secondLine[0]);
+        int volt =Integer.parseInt(secondLine[1]);
+
+        int adapter_ampere = watt / volt;
+
+        if (adapter_ampere >= tree_ampere) {
+            System.out.println(1);
+        }else System.out.println(0);
+    }
+}
