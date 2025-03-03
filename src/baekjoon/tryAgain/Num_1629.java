3cd24a645ac490b05e4bbf7ce871452e630cd6e6
jimin990 <wlals3236@naver.com>
2025-03-03 16:49:14 +0900

    add tryAgain

---
+++ src/baekjoon/tryAgain/Num_1629.java
@@ -0,0 +1,29 @@
+package baekjoon.tryAgain;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Num_1629 {
+    public static void main(String[] args) throws IOException {
+
+        /**
+         *
+         * A를 B번 곱하기에는 시간제한이 안됨
+         * A의 계산 횟수를 줄여야한다.
+         *
+         *
+         */
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+
+    }
+}
