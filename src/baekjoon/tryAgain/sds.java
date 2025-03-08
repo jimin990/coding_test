e6df3b721be430a12dcfb10ab95d0f2672618b2e
jimin990 <wlals3236@naver.com>
2025-03-08 22:10:45 +0900

    class 2

---
+++ src/baekjoon/tryAgain/sds.java
@@ -0,0 +1,36 @@
+package baekjoon.tryAgain;
+
+import java.io.*;
+
+public class sds {
+    static int num = 0;
+    static BufferedWriter bw;
+
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        sb = new StringBuilder();
+
+
+        func(1, 3, n);
+
+        System.out.println(num); // (1) 먼저 이동 횟수를 출력
+        System.out.println(sb);
+
+    }
+
+    static void func(int a, int b, int n) throws IOException {
+        if (n == 1) {
+            num++;
+            //bw.write(a + " " + b);
+            sb.append(a + " " + b+ "\n");
+            //bw.newLine();
+            return;
+        }
+
+        func(a, 6 - a - b, n - 1); // (1) n-1개의 원반을 보조 기둥으로 이동
+        func(a, b,1);
+        func(6 - a - b, b, n - 1); // (4) n-1개의 원반을 보조 기둥에서 목표 기둥으로 이동
+    }
+}
