4b2e76ff8b3b9b03a8f243b5f1b68aeb71026f21
jimin990 <wlals3236@naver.com>
2025-03-31 23:42:57 +0900

    moo

---
+++ src/baekjoon/bronze/Num_1890.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze;
+
+public class Num_1890 {
+    public static void main(String[] args) {
+
+        System.out.println("(___)");
+        System.out.println("(o o)____/");
+        System.out.println(" @@      \\");
+        System.out.println("  \\ ____,/");
+        System.out.println("  //   //");
+        System.out.println(" ^^   ^^");
+    }
+}
