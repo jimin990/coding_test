d833bc5bdaaa1fc5a23db4650254729dab27e476
jimin990 <wlals3236@naver.com>
2025-04-27 20:08:48 +0900

    1920 refactoring

--- src/baekjoon/silver/Num_1920.java
+++ src/baekjoon/silver/Num_1920.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
+/*
 public class Num_1920 {
     public static void main(String[] args) throws IOException {
 
@@ -36,5 +37,38 @@ public class Num_1920 {
 
         bw.flush();
         bw.close();
+    }
+}
+*/
+public class Num_1920 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sB = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        HashSet<Integer> set = new HashSet<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.valueOf(st.nextToken()));
+        }
+
+        int M = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < M; i++) {
+            if(set.contains(Integer.parseInt(st.nextToken()))){
+                sB.append("1\n");
+            }else{
+                sB.append("0\n");
+            }
+        }
+
+        System.out.print(sB);
     }
 }
