886acf0f7959e0d08c6881d84348c4380cb27658
jimin990 <wlals3236@naver.com>
2025-04-27 20:05:49 +0900

    58

---
+++ src/baekjoon/silver/Num_1920.java
@@ -0,0 +1,40 @@
+package baekjoon.silver;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Num_1920 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        HashSet<Integer> set = new HashSet<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.valueOf(st.nextToken()));
+        }
+
+        int M = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < M; i++) {
+            if(set.contains(Integer.parseInt(st.nextToken()))){
+                bw.write("1");
+            }else{
+                bw.write("0");
+            }
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
