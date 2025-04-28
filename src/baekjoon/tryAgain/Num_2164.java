f1f5ced64b2177b804e817c7c4c45d095037c2b1
jimin990 <wlals3236@naver.com>
2025-04-28 18:36:56 +0900

    59

---
+++ src/baekjoon/tryAgain/Num_2164.java
@@ -0,0 +1,35 @@
+package baekjoon.tryAgain;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class Num_2164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        LinkedList<Integer> list = new LinkedList<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            list.add(i);
+        }
+
+        while(list.size() != 1){
+            list.remove(0);
+
+            if(list.size() == 1){
+                break;
+            }
+
+            Integer i = list.get(0);
+            list.remove(0);
+            list.add(i);
+        }
+
+        System.out.println(list.get(0));
+
+    }
+}
