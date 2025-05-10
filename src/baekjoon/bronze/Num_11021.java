e1ce53d7222cc2dc35993377b6aa52c60cc3cdcb
jimin990 <wlals3236@naver.com>
2025-05-10 09:48:16 +0900

    71

---
+++ src/baekjoon/bronze/Num_11021.java
@@ -0,0 +1,28 @@
+package baekjoon.bronze;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Num_11021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+
+            bw.write("Case #" + i + ": " + (num1 + num2));
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+
+    }
+}
